ab5d8e9e6d6341007bec23ba92b0ab68019e8e1f
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-27 16:53:37 +0530

    header

---
+++ renting/src/Componets/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header className='bg-slate-200 shadow-lg'>
+        <div className=" flex justify-between items-center max-w-6xl mx-auto p-3 ">
+        <Link to='/'>
+        <h1 className=' font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Priyanshu</span>
+        <span className='text-slate-700'>Rentings</span>
+      </h1>
+      </Link>
+      <form className='flex flex-wrap bg-slate-100 p-3 rounded-lg '>
+        <input
+            type='text'
+            placeholder='Search...'
+            className='border border-gray-200 rounded-l px-1 py-1 focus:outline-none w-[70px] sm:w-[250px]'
+        />
+        <button className='bg-slate-500 text-white px-1 py-0.5 rounded-r focus:outline-none'>
+            Search
+        </button>
+        </form>
+        <ul className='flex gap-4'>
+            <Link to='/'>  
+            <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+            </Link>
+            <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+            </Link>
+            <Link to='/sign-in'>
+            <li >Sign in</li>
+            </Link>
+        </ul>
+        </div>
+    </header>
+  )
+}
+export default Header
