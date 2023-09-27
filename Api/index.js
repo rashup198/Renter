eb8d8c4c65c6ef0a61386fe0ba10a3d0628038bf
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-27 16:54:08 +0530

    index.js

---
+++ Api/index.js
@@ -0,0 +1,39 @@
+import express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
+
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
+});
+
+
+const app = express();  
+
+app.use(express.json());    
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`hello! Server is running on port ${port}`);
+}
+);
+
+
+app.use("/api/user", userRoute)
+app.use("/api/auth", authRoute)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+
+    });
+    });
+
