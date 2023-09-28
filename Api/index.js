6b8ff91722e1ec3390022fe47908e710ccb58170
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-28 13:21:17 +0530

    updated

--- Api/index.js
+++ Api/index.js
@@ -3,9 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
 import cookieParser from 'cookie-parser';
-import path from 'path';
+
 dotenv.config();
 
 mongoose
@@ -17,28 +16,20 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
 
-const app = express();
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
+  
+  app.use(cookieParser());
+  
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+  });
+  
 
-app.use(cookieParser());
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
-
-app.use('/api/user', userRouter);
+  app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
-
-
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -49,3 +40,4 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
