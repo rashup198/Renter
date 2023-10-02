02c2f801766ee367707f18b4487d7d4f1a02e4ee
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-10-02 11:23:57 +0530

    updated search api route

--- Api/routes/listing.route.js
+++ Api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing ,deleteListing,updateListing,getListing} from '../controlles/listing.controler.js';
+import { createListing ,deleteListing,updateListing,getListing,getListings} from '../controlles/listing.controler.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,4 +8,5 @@ router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing)
 router.get('/get/:id', getListing)
+router.get('/get', getListings)
 export default router;
