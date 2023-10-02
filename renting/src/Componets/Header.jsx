bea44d87fd63378a3fd57fd16080b6d45d187b25
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-10-02 11:36:48 +0530

    updated Search bar Funtion

--- renting/src/Componets/Header.jsx
+++ renting/src/Componets/Header.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import { Link} from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useNavigate} from 'react-router-dom'
 import { useSelector } from 'react-redux'
-
+import { useState } from 'react'
 
 
 const Header = () => {
   const {currentUser} = useSelector(state => state.user)
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
   
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+
+  }
   
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchtermFromUrl = urlParams.get('searchTerm');
+    if(searchtermFromUrl){
+      setSearchTerm(searchtermFromUrl);
+    }
+  }, [location.search]); 
 
   return (
     <header className='bg-slate-200 shadow-lg'>
@@ -18,11 +36,12 @@ const Header = () => {
         <span className='text-slate-700'></span>
       </h1>
       </Link>
-      <form className='flex flex-wrap bg-slate-100 p-3 rounded-lg '>
+      <form onSubmit={handleSubmit} className='flex flex-wrap bg-slate-100 p-3 rounded-lg '>
         <input
             type='text'
             placeholder='Search...'
-            
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
             className='border border-gray-200 rounded-l px-1 py-1 focus:outline-none w-[70px] sm:w-[250px]'
         />
         <button className='bg-slate-500 text-white px-1 py-0.5 rounded-r focus:outline-none'>
