c17de39776df753e131f01b56ef4628ca7adfb08
Priyanshu Kumar Pandey <88549100+rashup198@users.noreply.github.com>
2025-03-02 11:48:50 +0530

    updated server file

--- Api/index.js
+++ Api/index.js
@@ -7,41 +7,38 @@ import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 dotenv.config();
- 
 
+const PORT = process.env.PORT || 3000;
+ 
 mongoose.connect("mongodb+srv://rashup198:21Blc1377@cluster0.ak1ovd0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
   console.log('Successfully connected to the database');
 }).catch(err => {
-      console.log('Could not connect to the database. Exiting now...', err);
-      process.exit();
-  });
+  console.log('Could not connect to the database. Exiting now...', err);
+  process.exit();
+});
 
-  const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
-
+// Routes
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-
 
 app.use(express.static(path.join(__dirname, '/renting/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'renting', 'dist', 'index.html'));
-})
+});
 
+// Error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
@@ -50,4 +47,9 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
